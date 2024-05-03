3576be68ff0336489aed298217e6a4407d4363ca
Kyle Graham <kgra9230@uni.sydney.edu.au>
2024-05-04 03:07:28 +1000

    Initial Commit

---
+++ Emmet/EmmetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmmetApp.swift
+//  Emmet
+//
+//  Created by Kyle Graham on 4/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmmetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
